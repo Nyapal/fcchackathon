54efac88c1f07d07484f38a552c809643ec087d3
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-09-02 12:56:19 -0700

    free code camp hackathon

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>"My name is Nyapal Gatkuoth. I am a Full Stack Developer based in San Fransisco, CA. I am a junior at Make School and my favorite thing about coding is bringing my ideas to life."</p>
+  </div>
+)
+
+export default IndexPage
